e502b01224bf27394b92de33242cec6364070d39
RohitSM <rohitsmurdeshwar245@gmail.com>
2025-12-08 10:24:42 +0000

    DONE

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -28,3 +28,63 @@ let someNumber=33
 let numberToString=String(someNumber)
 console.log(numberToString);
 console.log(typeof numberToString)
+
+// **************************Operation*****************************
+
+let value=3
+let negativeValue=-value;
+console.log(negativeValue);
+
+console.log(2+2);
+console.log(2*2);
+console.log(2%2);
+console.log(2**2);
+console.log(2/2);
+console.log(2-2);
+
+
+let str1="WELCOME TO"
+let str2=" JAVASCRIPT WORLD"
+
+str3=str1+str2
+console.log(str3);
+
+
+console.log(2+"2");
+console.log("2"+2);
+console.log(2+2+"2");
+
+console.log((3+4)*5%3);
+
+console.log(+true);
+console.log(+"");
+
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+// PREFIX
+
+x=2
+y=++x
+console.log(x,y);
+
+x=2n
+y=++x
+console.log(x,y);
+
+// POSTFIX
+
+x=2
+y=x++
+console.log(x,y);
+
+x=2n
+y=x++
+console.log(x,y);
+
+
+
+
+
