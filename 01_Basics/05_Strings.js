fbd8112b617af4b759010c6dadacac34a6330194
RohitSM <rohitsmurdeshwar245@gmail.com>
2025-12-11 16:07:33 +0000

    About String

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,66 @@
+const name="Rohit"
+const course="JavaScript"
+
+console.log(name+course);//Dont use this syntax 
+
+console.log(`Hello this is ${name} and i am learning ${course}`);//use this syntax for more readablability
+
+const gameName=new String("GTA-V-Rohit");//Declaring a String Ussing Object Syntax
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('T'));
+
+
+const newString=gameName.substring(0,5);
+console.log(newString);//we cannot use negative indexing in substring
+
+const anotherString=gameName.slice(-9,5);
+console.log(anotherString);//can use negative indexing in slice
+
+const stringName="   Rohit   "
+console.log(stringName);
+console.log(stringName.trim());
+
+const url="https://www.RohitPortfolio.com//Rohit%20sm"
+console.log(url);
+console.log(url.replace('%20','-'));
+console.log(url.replaceAll("//","-"));
+
+
+console.log(url.includes("Rohit"));
+console.log(url.includes("rohit"));
+
+console.log(gameName.split("-"));
+console.log(gameName.split("-",2));
+
+console.log(gameName.at(1,4));
+
+console.log(gameName.charCodeAt(3,7))//gives the unicode value of character at given index
+
+console.log(gameName.codePointAt(0,4));
+
+console.log(gameName.concat());
+
+console.log(gameName.toLocaleUpperCase());
+
+console.log(gameName.isWellFormed());
+
+
+
+
+
+
+
+
+
+
+
+
