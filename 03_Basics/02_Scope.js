2518221fb982a94943fd4d9c5c93d7306566041d
RohitSM <rohitsmurdeshwar245@gmail.com>
2025-12-23 20:01:48 +0000

    Scopes

--- 03_Basics/02_Scope.js
+++ 03_Basics/02_Scope.js
@@ -12,3 +12,47 @@ console.log("Inner is",a);
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+//++++++++++++nested scope++++++++++++++
+
+function one(){
+    const username="Rohit"
+
+    function two(){
+        const website="YouTube"
+        console.log(username);
+    }
+    // console.log(website);//this will not execute because it is not defined and it is outside the scope
+    
+    two()
+}
+one()
+
+
+if(true){
+    const username="Rohit"
+if(username==="Rohit"){
+    const website="Youtube"
+    console.log(username+website);
+    
+}
+// console.log(website);//this will not execute due to it is outside the scope
+
+}
+// console.log(username);//this will not execute due to it is outside the scope
+
+
+//++++++++++++++++Interesting++++++++++++++
+
+console.log(addOne(5))//this will execute
+function addOne(num){ //this is the type of function is created
+    return num + 1
+    
+}
+
+// console.log(addTwo(5));//this cannot access addTwo before initialization due to the holding (hoisting) it on a variable
+const addTwo=function(num){//this is another type of creating a function
+    return num + 2
+}
+console.log(addTwo(5));//it will execute
