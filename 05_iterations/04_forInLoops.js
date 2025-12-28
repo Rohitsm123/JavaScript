ab5efcd642f786ba8a3d2f447de71e12eb271df7
RohitSM <rohitsmurdeshwar245@gmail.com>
2025-12-28 16:45:04 +0000

    for in loops & Maps

---
+++ 05_iterations/04_forInLoops.js
@@ -0,0 +1,48 @@
+//for In syntax:
+
+// for (const key in object) {
+    
+// }
+
+
+const myObj={
+    js:"JavaScript",
+    py:"Python",
+    cpp:"C++",
+    java:"Java"
+}
+
+for (const key in myObj) {//for in loop to iterate over object properties
+    console.log(key);
+  console.log(`${key} Shortcut for: ${myObj[key]}`);//accessing values using keys
+
+}
+
+
+const proLanguages=['JavaScript','Python','C++','Java','Ruby']
+
+for (const key in proLanguages) {//for in loop to iterate over array indices
+ 
+    console.log(`${proLanguages[key]} is in ${key} index`);//accessing values using index
+   
+}
+
+const arr=['a','b','c','d','e']
+
+for(const index in arr){//for in loop to iterate over array indices
+    console.log(index,arr[index]);//prints index and corresponding value
+    
+}
+
+
+
+const myMap=new Map()
+myMap.set('IN','India')
+myMap.set('US','United States')
+myMap.set('FR','France')
+myMap.set('IN','India')
+
+for (const key in myMap) {//for in loop doesnot work with maps because maps are not enumerable
+    console.log(key);
+    
+}
